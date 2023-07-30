acb63b21cd92acd7ddefec311be812df1c877db0
o0o0o0o0o0o0o0o0o0o0o0 <140931617+o0o0o0o0o0o0o0o0o0o0o0@users.noreply.github.com>
2023-07-30 13:38:06 +0900

    Create getUserInfo.py
    add 코드페어 프로젝트

---
+++ 코드페어 프로젝트/getUserInfo.py
@@ -0,0 +1,40 @@
+import pymysql
+conn = pymysql.connect(host = "localhost", db = "mask", user ="root", password = "jennyyoon0814")
+
+def getIdPass():
+
+    cur = conn.cursor()
+
+    sql = "select id, pw from members"
+
+    cur.execute(sql)
+
+    total = []
+    
+    for i in cur:
+        result = {}
+        result["id"] = i[0]
+        result["password"] = i[1]
+
+        total.append(result)
+    
+    return total
+
+def getIdPassByCondition(Id):
+
+    cur = conn.cursor()
+
+    sql = "select id, pw from members where `id` like '%s%'" % (Id)
+
+    cur.execute(sql)
+
+    total = []
+    
+    for i in cur:
+        result = {}
+        result["id"] = i[0]
+        result["password"] = i[1]
+
+        total.append(result)
+    
+    return total
