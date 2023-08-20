72fc12969a5e4c163d92967018c7063b30c3c2cc
o0o0o0o0o0o0o0o0o0o0o0 <140931617+o0o0o0o0o0o0o0o0o0o0o0@users.noreply.github.com>
2023-08-20 14:29:43 +0900

    Update app.py
    쿼리 파라미터 -> 패스 파라미터 변경
    body로 받아오는 거 추가

--- 코드페어 프로젝트/app.py
+++ 코드페어 프로젝트/app.py
@@ -10,10 +10,9 @@ def users():
     }
     return message
 
-@app.route("/users/by-condition" , methods=["GET"])
-def usersByCondition():
-    p = gui.getIdPassByCondition(request.args.get("id"))
-    print(p)
+@app.route("/users/by-condition/<int:id>" , methods=["GET"])
+def usersByCondition(id):
+    p = gui.getIdPassByCondition(id)
     message = {
         "result":p
     }
@@ -21,26 +20,36 @@ def usersByCondition():
 
 @app.route("/users/signup" , methods=["POST"])
 def usersSignup():
-    p = gui.postMemberSignUp(request.args.get("id"), request.args.get("pw"), request.args.get("tel"), request.args.get("sex"), request.args.get("age"))
-    print(p)
+
+    request_body = request.get_json()
+    userid = request_body["id"]
+    pw = request_body["pw"]
+    tel = request_body["tel"]
+    sex = request_body["sex"]
+    age = request_body["age"]
+
+    p = gui.postMemberSignUp(userid, pw, tel, sex, age)
     message = {
         "result":p
     }
     return message
 
-@app.route("/users/change" , methods=["PUT"])
-def usersChange():
-    p = gui.putChangeContent(request.args.get("id"), request.args.get("mainAgent"), request.args.get("changed"))
-    print(p)
+@app.route("/users/change/<int:id>" , methods=["PUT"])
+def usersChange(id):
+    requestBody = request.get_json()
+    mainAgent = requestBody["mainAgent"]
+    changed = requestBody["changed"]
+
+    p = gui.putChangeContent(id, mainAgent, changed)
     message = {
         "result":p
     }
     return message
 
-@app.route("/users/delete" , methods=["Delete"])
-def usersDelete():
-    p = gui.Delete(request.args.get("id"))
-    print(p)
+@app.route("/users/delete/<int:id>" , methods=["Delete"])
+def usersDelete(id):
+    
+    p = gui.Delete(id)
     message = {
         "result":p
     }
