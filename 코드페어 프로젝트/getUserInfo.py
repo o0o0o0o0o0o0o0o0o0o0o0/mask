148653316de6462aca5163a8d2a890ed2612092a
moou814 <140931617+o0o0o0o0o0o0o0o0o0o0o0@users.noreply.github.com>
2023-10-15 13:44:05 +0900

    Update getUserInfo.py
    수정 명령어 수정(회원정보, 게시글)

--- 코드페어 프로젝트/getUserInfo.py
+++ 코드페어 프로젝트/getUserInfo.py
@@ -29,23 +29,19 @@ def getIdPassByCondition(Id):
 
     cur = conn.cursor()
 
-    sql = "select * from members where `id` like '%" + Id + "%'"
+    sql = f"select * from members where `id` = {Id}"
     cur.execute(sql)
+    cur = tuple(cur)
 
-    total = []
+    result = {}
+    result["id"] = cur[0][0]
+    result["userId"] = cur[0][1]
+    result["password"] = cur[0][2]
+    result["tel"] = cur[0][3]
+    result["sex"] = cur[0][4]
+    result["age"] = cur[0][5]
     
-    for i in cur:
-        result = {}
-        result["id"] = i[0]
-        result["userId"] = i[1]
-        result["password"] = i[2]
-        result["tel"] = i[3]
-        result["sex"] = i[4]
-        result["age"] = i[5]
-
-        total.append(result)
-    
-    return total
+    return result
 
 def postMemberSignUp(userid, pw, tel, sex, age) :
     if None in [userid, pw, tel, sex, age] :
@@ -62,22 +58,34 @@ def postMemberSignUp(userid, pw, tel, sex, age) :
             else : return '전화번호 입력 형식은 [01X-XXX(X)-XXXX]입니다. 확인하세요.'
         else : return '성별은 여성(w)과 남성(m)만 선택할 수 있습니다.'
 
-def putChangeContent(id, mainAgent, changed) :
-    cur = conn.cursor()
-    f = False
-    if None in [id, mainAgent, changed] :
-        return '값을 입력하세요.'
-    else :
-        if mainAgent == 'age' : sql = f"update `mask`.`members` set `{mainAgent}` = {int(changed)} where `id` = {id};"
-        else:
-            if bool(changed == ['w', 'm'] and mainAgent == 'sex') or bool(re.compile("^(01)\d{1}-\d{3,4}-\d{4}$").search(changed.replace(" ", "")) and mainAgent == 'tel') or mainAgent in ['userid, pw']:
-                f = True
+def putChangeContent(id, request) :
+    # 기본적으로 update를 모든 필드를 바꾸게끔 (id만 빼고)
+    # id로 유저를 조회
 
-        if f :
-            sql = f"update `mask`.`members` set `{mainAgent}` = '{changed}' where `id` = {id};"
-            cur.execute(sql)
+    cur = conn.cursor()
+
+    sql = f"select * from members where `id` = {id}"
+    cur.execute(sql)
+    cur = tuple(cur)
+
+    base = {
+        "userId": cur[0][1],
+        "pw":cur[0][2],
+        "tel":cur[0][3],
+        "sex":cur[0][4],
+        "age":cur[0][5]
+    }  
+
+    for key, value in request.items():
+        if key in base : 
+            base[key] = value
+
+
+    cur = conn.cursor()
+    sql = f"update `mask`.`members` set `userId` = '{base['userId']}', `pw` = '{base['pw']}', `tel` = '{base['tel']}', `sex` = '{base['sex']}', `age` = {base['age']} where `id` = {id};"
+    cur.execute(sql)
     
-            return 'completion'
+    return 'completion'
 
 def Delete(userid) :
     cur = conn.cursor()
